76d9d31ba94547de562ce7136b09fefe88374997
Mikalai19 <bertman29@gmail.com>
2022-05-06 16:58:15 -0700

    session secret changed

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 app.use(session({
-  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  secret: process.env.SESSION_SECRET,    // What we actually will be giving the user on our site as a session cookie
   resave: false,             // Save the session even if it's modified, make this false
   saveUninitialized: true    // If we have a new session, we save it, therefore making that true
 }));
