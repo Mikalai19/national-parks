d578098f2a66fe49c4f7668851615c4973eb42d0
Mikalai19 <bertman29@gmail.com>
2022-05-06 17:08:54 -0700

    gkg

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(layouts);
 app.use(session({
   secret: process.env.SESSION_SECRET,    // What we actually will be giving the user on our site as a session cookie
   resave: false,             // Save the session even if it's modified, make this false
-  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+  saveUninitialized: true,
+  cookie: { secure: false }   // If we have a new session, we save it, therefore making that true
 }));
 
 app.use(flash());
