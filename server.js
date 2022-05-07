f702522cc99ee8b56f03f276968d3532be3f18b8
Mikalai19 <bertman29@gmail.com>
2022-05-06 17:13:46 -0700

    gkg

--- server.js
+++ server.js
@@ -23,10 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 app.use(session({
-  secret: process.env.SESSION_SECRET,    // What we actually will be giving the user on our site as a session cookie
+  secret: 'secretidhere',    // What we actually will be giving the user on our site as a session cookie
   resave: false,             // Save the session even if it's modified, make this false
-  saveUninitialized: true,
-  cookie: { secure: false }   // If we have a new session, we save it, therefore making that true
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
 }));
 
 app.use(flash());
